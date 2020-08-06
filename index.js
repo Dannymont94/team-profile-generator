3f8e478c7c91ceca0ceda36e1684df3aeef577a3
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-06 01:13:24 -0400

    added mockData to index file to test generate-html function more easily

--- index.js
+++ index.js
@@ -9,7 +9,38 @@ const { promptManager, promptTeam } = require('./utils/prompt-user');
 //   .then(console.log('Team Profile page generated in dist folder!'))
 //   .catch(err => console.log(err));
 
-promptManager()
-  .then(teamData => promptTeam(teamData))
-  .then(teamData => generateHTML(teamData))
-  .catch(err => console.log(err));
+mockData = {
+  manager: {
+    name: 'Dan',
+    id: 'D123',
+    email: 'dan@dan.com',
+    officeNumber: 'f24'
+  },
+  engineers: [
+    {
+      role: 'Engineer',
+      name: 'Nes',
+      id: 'N123',
+      email: 'nes@nes.com',
+      github: 'neshub',
+      confirmAddTeamMember: true
+    }
+  ],
+  interns: [
+    {
+      role: 'Intern',
+      name: 'Bub',
+      id: 'B123',
+      email: 'bub@bub.com',
+      school: 'UB',
+      confirmAddTeamMember: false
+    }
+  ]
+};
+
+// promptManager()
+//   .then(teamData => promptTeam(teamData))
+//   .then(teamData => generateHTML(teamData))
+//   .catch(err => console.log(err));
+
+generateHTML(mockData);
--- src/generate-html.js
+++ src/generate-html.js
@@ -8,6 +8,10 @@ function generateInternHTML() {
 
 function generateHTML(teamData) {
   console.log(teamData);
+  const { manager, engineers, interns } = teamData;
+  console.log(manager);
+  console.log(engineers);
+  console.log(interns);
 }
 
 module.exports = generateHTML;
