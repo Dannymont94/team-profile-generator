95fc20bd3fc67e6908baedd3d6a338636219c4d4
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-08-07 20:32:26 -0400

    removed mock inquirer data

--- index.js
+++ index.js
@@ -2,52 +2,6 @@ const generateHTML = require('./src/generate-html');
 const { writeFile, copyFile } = require('./utils/write-and-copy-file');
 const { promptManager, promptTeam } = require('./utils/prompt-user');
 
-
-mockData = {
-  manager: {
-    name: 'Dan',
-    id: 'D123',
-    email: 'dan@dan.com',
-    officeNumber: 'f24'
-  },
-  engineers: [
-    {
-      role: 'Engineer',
-      name: 'Nes',
-      id: 'N123',
-      email: 'nes@nes.com',
-      github: 'neshub',
-      confirmAddTeamMember: true
-    },
-    {
-      role: 'Engineer',
-      name: 'Bec',
-      id: 'B123',
-      email: 'bec@bec.com',
-      github: 'bechub',
-      confirmAddTeamMember: true
-    }
-  ],
-  interns: [
-    {
-      role: 'Intern',
-      name: 'Bub',
-      id: 'B123',
-      email: 'bub@bub.com',
-      school: 'UB',
-      confirmAddTeamMember: false
-    },
-    {
-      role: 'Intern',
-      name: 'Man',
-      id: 'M123',
-      email: 'man@man.com',
-      school: 'UM',
-      confirmAddTeamMember: false
-    }
-  ]
-};
-
 promptManager()
   .then(teamData => promptTeam(teamData))
   .then(teamData => generateHTML(teamData))
